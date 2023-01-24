ceb0521afca9ec938ec3df7b04c138aaf3d2b6c5
Ocholi Joseph <ocholijoseph@gmail.com>
2023-01-24 13:15:29 -0800

    printf collaborative task

--- _printf.c
+++ _printf.c
@@ -16,7 +16,8 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-va_start(list, format);
+
+	va_start(list, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
